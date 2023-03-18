d9af301c42cfce9c1de31f227ccd908335f4c4d2
Brandon Admire <admireb@oregonstate.edu>
2023-03-18 14:30:25 -0700

    Added TaskTemplateDao

--- app/src/main/java/com/example/lifetracker/data/TaskTemplateDao.kt
+++ app/src/main/java/com/example/lifetracker/data/TaskTemplateDao.kt
@@ -1,4 +1,19 @@
 package com.example.lifetracker.data
 
-class TaskTemplateDao {
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskTemplateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(record: TaskTemplate)
+
+    @Delete
+    suspend fun delete(record: TaskTemplate)
+
+    @Query("SELECT * FROM TaskTemplate")
+    fun getAllTasks(): Flow<List<TaskTemplate>>
+
+    @Query("SELECT * FROM TaskTemplate WHERE name = :name LIMIT 1")
+    fun getTaskByName(name:String?): Flow<TaskTemplate>
 }
