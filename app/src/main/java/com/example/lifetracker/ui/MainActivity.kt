baf6e94e3625c362f7279b109eca8eac5a104551
Brandon Admire <admireb@oregonstate.edu>
2023-03-18 14:44:10 -0700

    Skeleton-ed broken MainActivity.kt

--- app/src/main/java/com/example/lifetracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/lifetracker/ui/MainActivity.kt
@@ -1,4 +1,50 @@
 package com.example.lifetracker.ui
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.myapplication.R
+import com.google.android.material.navigation.NavigationView
+
+//TODO: Connect with layouts and nav fragment
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Feed in layout container for jetbrains
+        setContentView(R.layout.activity_main)
+
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Instantiate nav fragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment
+            ) as NavHostFragment
+
+        // Get hooks for nav controller
+        val navController = navHostFragment.navController
+
+        // Define layout for nav drawer
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        // Hook up app bar with nav drawer layout
+        appBarConfig = AppBarConfiguration(navController.graph,drawerLayout)
+
+        // Hook up nav controller and appbar/drawer
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        // Attach nav view to viewmodel
+        findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController =
+            findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfig)
+                || super.onSupportNavigateUp()
+    }
 }
