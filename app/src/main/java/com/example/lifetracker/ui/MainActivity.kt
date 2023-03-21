2d4706d19e849af20ee3be95dfc2c69a73e12dc1
justinflesch <fleschju@oregonstate.edu>
2023-03-21 14:59:44 -0700

    Google One Tap sign-in might be working

--- app/src/main/java/com/example/lifetracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/lifetracker/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.lifetracker.ui
 
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -19,23 +21,15 @@ import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Scope
 import com.google.android.material.internal.NavigationMenu
 
-//private fun GoogleSignIn() {
-//
-//    if (!isUserSignedIn()) {
-//        val gso = GoogleSignInOptions
-//            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestEmail()
-//            .requestProfile()
-//            .requestScopes(Scope(DriveScopes.DRIVE))
-//            .build()
-//    }
-//
-//}
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     // Container for nav drawer/app bar
     private lateinit var appBarConfig: AppBarConfiguration
@@ -43,6 +37,56 @@ class MainActivity : AppCompatActivity() {
     // Containers for GoogleDrive stuff
     private lateinit var oneTapClient: SignInClient
     private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d(TAG, "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    when (e.statusCode) {
+                        CommonStatusCodes.CANCELED -> {
+                            Log.d(TAG, "One-tap dialog was closed.")
+                            // Don't re-prompt the user.
+                            showOneTapUI = false
+                        }
+                        CommonStatusCodes.NETWORK_ERROR -> {
+                            Log.d(TAG, "One-tap encountered a network error.")
+                            // Try again or just ignore.
+                        }
+                        else -> {
+                            Log.d(TAG, "Couldn't get credential from result." +
+                                    " (${e.localizedMessage})")
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,29 +120,48 @@ class MainActivity : AppCompatActivity() {
         findViewById<NavigationView>(R.id.nav_view)?.setupWithNavController(navController)
 
         var menu = findViewById<NavigationView>(R.id.nav_view)?.menu
-//        val exportButtonItem = menu!!.add("Export Data")
-//        exportButtonItem.setOnMenuItemClickListener {
-//
-//            oneTapClient = Identity.getSignInClient(this)
-//            signInRequest = BeginSignInRequest.builder()
-//                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-//                    .setSupported(true)
-//                    .build())
-//                .setGoogleIdTokenRequestOptions(
-//                    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                        .setSupported(true)
-//                        // Your server's client ID, not your Android client ID.
-//                        .setServerClientId(getString(R.string.your_web_client_id))
-//                        // Only show accounts previously used to sign in.
-//                        .setFilterByAuthorizedAccounts(true)
-//                        .build())
-//                // Automatically sign in when exactly one credential is retrieved.
-//                .setAutoSelectEnabled(true)
-//                .build()
-//            // ...
-//
-//            return@setOnMenuItemClickListener true
-//        }
+        val exportButtonItem = menu!!.add("Export Data")
+        exportButtonItem.setOnMenuItemClickListener {
+
+            // Set up objects to handle user sign-in
+            oneTapClient = Identity.getSignInClient(this)
+            signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build())
+                .setGoogleIdTokenRequestOptions(
+                    BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.your_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build()
+
+            // Display Google's one tap sign-in UI
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this) { result ->
+                    try {
+                        startIntentSenderForResult(
+                            result.pendingIntent.intentSender, REQ_ONE_TAP,
+                            null, 0, 0, 0, null)
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener(this) { e ->
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d(TAG, e.localizedMessage)
+                }
+
+
+
+            return@setOnMenuItemClickListener true
+        }
     }
 
 //    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
